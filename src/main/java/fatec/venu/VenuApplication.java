9522e19e8cd716d92af001d1b41b778a20004381
Andrea <andreamoraes12@gmail.com>
2026-07-13 21:12:18 -0300

    Reenviando projeto para git

---
+++ src/main/java/fatec/venu/VenuApplication.java
@@ -0,0 +1,13 @@
+package fatec.venu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenuApplication.class, args);
+	}
+
+}
